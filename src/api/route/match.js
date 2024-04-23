f4ec8e45914d7a5d719b36dd40be972208df820e
hoatruong <hoatruongdev09@gmail.com>
2024-04-23 14:39:39 +0700

    add validate token api

--- src/api/route/match.js
+++ src/api/route/match.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { getMatchInfo } from "../../provider/match.provider.js";
+import { getMatchInfo, findPlayerInMatch } from "../../provider/match.provider.js";
+import { jwtDecode } from "jwt-decode";
 
 const router = Router()
 
@@ -10,6 +11,22 @@ router.get('/:id', async (req, res) => {
         res.status(200).json({
             status: true,
             data: await getMatchInfo(id)
+        })
+    } catch (err) {
+        res.status(500).json({
+            status: false,
+            error: err.message
+        })
+    }
+})
+
+router.post('/validate-token', async (req, res) => {
+    const { token, match_id } = req.body
+    const { user_id } = jwtDecode(token)
+    try {
+        res.status(200).json({
+            status: true,
+            data: await findPlayerInMatch(user_id, match_id)
         })
     } catch (err) {
         res.status(500).json({
--- src/provider/match.provider.js
+++ src/provider/match.provider.js
@@ -1,4 +1,6 @@
 import db from '../db/db.js'
+import MatchPlayer from '../db/entity/match.player.js'
+import Match from '../db/entity/match.js'
 
 export const createMatchInfo = async (matchMode, instanceHost, instancePort) => {
     try {
@@ -31,7 +33,6 @@ export const getMatchInfo = async (matchId) => {
 
 export const addPlayerToMatch = async (matchId, players) => {
     try {
-        console.log(players)
         const repo = db.getRepository("MatchPlayer")
         const matchPlayers = players.map(player => repo.save({
             ...player,
@@ -40,5 +41,17 @@ export const addPlayerToMatch = async (matchId, players) => {
         return await Promise.all(matchPlayers)
     } catch (err) {
         throw err
+    }
+}
+
+export const findPlayerInMatch = async (playerId, matchId) => {
+    try {
+        const info = await db.manager
+            .createQueryBuilder(MatchPlayer, "player")
+            .where("player.match_id = :matchId AND player.user_id = :playerId", { matchId: matchId, playerId: playerId })
+            .getOne()
+        return info
+    } catch (error) {
+        throw error
     }
 }
