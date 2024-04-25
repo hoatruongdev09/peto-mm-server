3c498f0cbb604f2d9203ecf7ebded83c353ab3e6
hoatruong <hoatruongdev09@gmail.com>
2024-04-25 17:25:53 +0700

    update server build

--- src/game_instance/game-runner.js
+++ src/game_instance/game-runner.js
@@ -2,6 +2,7 @@ import net from 'net'
 import { spawn } from 'child_process'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+const fs = require('node:fs');
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -40,30 +41,49 @@ const createHostData = () => {
         port
     }
 }
+const saveInstanceLog = (name, logs) => {
+    const content = logs.join('\n')
+    fs.writeFile(`./logs/${name}.txt`, content, err => {
+        if (err) {
+            console.error('save log error', err);
+        } else {
+            console.log('save logs successful')
+        }
+    });
+}
 const createInstance = (gameId, host, port) => {
 
     const args = ["-game_id", `${gameId}`, "-host", `${host}`, "-port", `${port}`]
     try {
 
+        const logName = `log_${game_id}`;
+        const logs = []
         const child = spawn(linuxProcessPath, args)
 
         child.stdout.on("data", (data) => {
-            console.log(`match ${gameId} ${port} data: ${data}`);
-        });
+            logs.push(`match ${gameId} ${port} data: ${data}`)
+            console.log(`match ${gameId} ${port} data: ${data}`)
+        })
         child.on("spawn", () => {
-            console.log(`match ${gameId} ${port} spawned`);
-        });
+            logs.push(`match ${gameId} ${port} spawned`)
+            console.log(`match ${gameId} ${port} spawned`)
+        })
         child.on("disconnect", (code, signal) => {
-            console.log(`match ${gameId} ${port} closed ${code} ${signal}`);
-        });
+            logs.push(`match ${gameId} ${port} closed ${code} ${signal}`)
+            console.log(`match ${gameId} ${port} closed ${code} ${signal}`)
+        })
         child.on("message", (code, signal) => {
-            console.log(`match ${gameId} ${port} message ${code} ${signal}`);
-        });
+            logs.push(`match ${gameId} ${port} message ${code} ${signal}`)
+            console.log(`match ${gameId} ${port} message ${code} ${signal}`)
+        })
         child.on("error", (code, signal) => {
-            console.log(`match ${gameId} ${port} error ${code} ${signal}`);
-        });
+            logs.push(`match ${gameId} ${port} error ${code} ${signal}`)
+            console.log(`match ${gameId} ${port} error ${code} ${signal}`)
+        })
         child.on("exit", (code, signal) => {
-            console.log(`match ${gameId} ${port} exit ${code} ${signal}`);
+            logs.push(`match ${gameId} ${port} exit ${code} ${signal}`)
+            console.log(`match ${gameId} ${port} exit ${code} ${signal}`)
+            saveInstanceLog(logName, logs)
             portMap.set(port, false)
         });
 
